a86a6bf6b7548c1b44dadb2a7b45f2abba8158b6
smar152 <smar152@users.noreply.github.com>
2020-02-26 18:37:34 +0200

    Updates

---
+++ precache-manifest.eb81b2b1635bc2cc2400e6046bcc5707.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7514d66eca9c646e961d15585233065",
+    "url": "/smarmakescomics/index.html"
+  },
+  {
+    "revision": "35f010eceb887b0d5d04",
+    "url": "/smarmakescomics/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "47c8cde17094eb4d9188",
+    "url": "/smarmakescomics/static/js/2.e3f27d42.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/smarmakescomics/static/js/2.e3f27d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f010eceb887b0d5d04",
+    "url": "/smarmakescomics/static/js/main.f60cb5a3.chunk.js"
+  },
+  {
+    "revision": "441f1efb126371de6bea",
+    "url": "/smarmakescomics/static/js/runtime-main.1d3b76af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smarmakescomics/static/media/logo.5d5d9eef.svg"
+  }
+]);
