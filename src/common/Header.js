3291b348823f8f5dada67048125b1b78ee808d68
smar152 <smar152@users.noreply.github.com>
2020-02-29 20:01:22 +0200

    Added header, footer, router, illustration gallery page

---
+++ src/common/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class Header extends Component {
+  state = {};
+  render() {
+    return <div className="header"></div>;
+  }
+}
+
+export default Header;
