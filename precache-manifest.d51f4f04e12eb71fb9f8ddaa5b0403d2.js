d2e72eeb39b32f3ab2978dc0945eb4c88ecc7839
smar152 <smar152@users.noreply.github.com>
2020-03-12 22:42:15 +0200

    Updates

---
+++ precache-manifest.d51f4f04e12eb71fb9f8ddaa5b0403d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046127d5101e00958a570749863ed1c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "956a0deabc3557235367",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "d3bc877233f2089f4bd9",
+    "url": "/static/css/main.67e0173c.chunk.css"
+  },
+  {
+    "revision": "956a0deabc3557235367",
+    "url": "/static/js/2.017b9098.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.017b9098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3bc877233f2089f4bd9",
+    "url": "/static/js/main.2a0fde0c.chunk.js"
+  },
+  {
+    "revision": "2dbec2b1c90c5e5f7c97",
+    "url": "/static/js/runtime-main.a6b9e736.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/smarmakescomics/precache-manifest.67a7aa3fc67c3a180be06d54e83fb4c6.js"
+  "/precache-manifest.d51f4f04e12eb71fb9f8ddaa5b0403d2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/smarmakescomics/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
