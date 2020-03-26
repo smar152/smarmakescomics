c54a8d47fffcc91afbe81bbae59030d85a50cce3
smar152 <smar152@users.noreply.github.com>
2020-03-26 14:53:25 +0200

    Updates

--- precache-manifest.576b8c992afd56c5e76cc923a45d2759.js
+++ precache-manifest.576b8c992afd56c5e76cc923a45d2759.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2a2751f1710a91a9c6c2aaf2cbe957bd",
+    "revision": "2febdf7d05e66182daabf8266c80a5e4",
     "url": "/index.html"
   },
   {
-    "revision": "956a0deabc3557235367",
+    "revision": "98479a04f7b1c7ddac7a",
     "url": "/static/css/2.67123b20.chunk.css"
   },
   {
-    "revision": "4b6284db6e84c646d33b",
+    "revision": "794255e06406038ecd7c",
     "url": "/static/css/main.538ccaaf.chunk.css"
   },
   {
-    "revision": "956a0deabc3557235367",
-    "url": "/static/js/2.017b9098.chunk.js"
+    "revision": "98479a04f7b1c7ddac7a",
+    "url": "/static/js/2.7c77e0dc.chunk.js"
   },
   {
     "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/static/js/2.017b9098.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.7c77e0dc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4b6284db6e84c646d33b",
-    "url": "/static/js/main.e2636f40.chunk.js"
+    "revision": "794255e06406038ecd7c",
+    "url": "/static/js/main.5fbe1c02.chunk.js"
   },
   {
     "revision": "2dbec2b1c90c5e5f7c97",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2a99910703870cc381b16ee792e14e2.js"
+  "/precache-manifest.576b8c992afd56c5e76cc923a45d2759.js"
 );
 
 self.addEventListener('message', (event) => {
